ac641e5396463e0d41c626094763234dca4c30f0
eeue56 <enalicho@gmail.com>
2016-04-11 13:53:41 +0300

    use jeg2 suggestions

--- elm_phoenixed/web/controllers/page_controller.ex
+++ elm_phoenixed/web/controllers/page_controller.ex
@@ -8,33 +8,37 @@ defmodule ElmPhoenixed.PageController do
   def gather_args(nil) do [] end
   def gather_args(action) do gather_args(action, 0) end
   def gather_args(action, index) do
-    current_index = "_" <> (to_string index)
+    current_index = "_#{index}"
     value = Map.get(action, current_index)
 
     if value == nil do
-      []
+      [ ]
     else
-      [ value ] ++ gather_args(action, index + 1)
+      [ value | gather_args(action, index + 1) ]
     end
   end
 
   @spec run_action(char_list, list, number) :: number
-  def run_action("Sub", args, model) do model - Enum.sum(args) end
-  def run_action("Add", args, model) do model + Enum.sum(args) end
-  def run_action("RecordAdd", [], model) do model end
-  def run_action("RecordAdd", [ head | tail ], model) do
-    add = Map.get(head, "add", 0)
+  def run_action("Sub", model, n) do model - n end
+  def run_action("Add", model, n) do model + n end
+  def run_action("AddTwo", model, n, m) do n + m end
+  def run_action("RecordAdd", model, record) do
+    add = Map.get(record, "add", 0)
     model + add
   end
-  def run_action(_, _, model) do model end
+  def run_action(_, model, _) do model end
 
   def api(conn, params) do
     action = Map.get(params, "action")
     args = gather_args(action)
     model = Map.get(params, "model")
 
+    IO.inspect action
+
     action_name = Map.get(action, "ctor")
 
-    json conn, %{model: run_action(action_name, args, model)}
+    full_arguments = [ action_name, model ] ++ args
+
+    json conn, %{model: apply(ElmPhoenixed.PageController, :run_action, full_arguments)}
   end
 end
